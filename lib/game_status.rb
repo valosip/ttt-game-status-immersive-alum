e6bb08d6f3b9c3d41faea7b55aa3724e95f3cdf6
Val Osip <valosip@gmail.com>
2020-04-03 01:41:18 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,12 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
-    if board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && combo[0] != ""
+    if (board[combo[0]] == "X" 
+      && board[combo[1]] == "X" 
+      && board[combo[2]] == "X") 
+      || (board[combo[0]] == "O" 
+      && board[combo[1]] == "O" 
+      && board[combo[2]] == "O") 
       return combo
     end
   end
